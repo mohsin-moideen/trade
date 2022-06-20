f60446d34178de0b3f86a2141a624e0b55ddf927
Mohsin Moideen <34484831+mohsin-moideen@users.noreply.github.com>
2022-06-20 09:46:54 +0530

    Update start-bot.sh

--- start-bot.sh
+++ start-bot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mvn compile assembly:single
-nohup  java -jar ./target/trade-0.1-jar-with-dependencies.jar &
+sudo nohup  java -jar ./target/trade-0.1-jar-with-dependencies.jar &
 export BOT_PID=$!
 echo Bot running on process ${BOT_PID}
 exit 0
