fa834e9632816fbc5820be6ec2730d8ad78ba8a2
Mohsin Moideen <34484831+mohsin-moideen@users.noreply.github.com>
2022-07-02 14:29:25 +0530

    refactoring telegram listener

--- src/main/java/org/trade/utils/TelegramUtils.java
+++ src/main/java/org/trade/utils/TelegramUtils.java
@@ -1,10 +1,14 @@
 package org.trade.utils;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.trade.App;
 
 import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.TelegramBot;
@@ -71,5 +75,100 @@ public class TelegramUtils {
 		while (true) {
 			Thread.sleep(1000);
 		}
+	}
+
+	public static void setTelegramListener(final Map<String, App> apps) {
+		final String STRATEGY = "STRATEGY";
+		final String BOT = "BOT";
+		final String PAUSE = "PAUSE";
+		final String RESUME = "RESUME";
+
+		setUpdatesListener(new UpdatesListener() {
+			@Override
+			public int process(List<Update> updates) {
+
+				System.out.println(updates);
+				for (Update update : updates) {
+					String[] tokens = update.message().text().trim().split(" ");
+					String auth = tokens[tokens.length - 1];
+					String fromUser = update.message().from().username();
+					String chatId = update.message().chat().id().toString();
+
+					if (!isAuthValid(auth, fromUser)) {
+						sendMessage("Invalid auth");
+						sendMessage("Requested by -> " + update.message().from());
+						sendMessage("Requested via chat -> " + update.message().chat());
+						sendMessage("Requested message -> " + update.message().text());
+						break;
+					}
+					if (tokens[0].equals(BOT)) {
+						switch (tokens[1]) {
+						case PAUSE: {
+							for (App app : apps.values()) {
+								app.pause();
+							}
+							sendMessage("Bot paused by " + fromUser);
+							break;
+
+						}
+						case RESUME: {
+							for (App app : apps.values()) {
+								app.resume();
+							}
+							sendMessage("Bot resumed by " + fromUser);
+							break;
+
+						}
+						default: {
+							sendMessage("Invalid action " + tokens[1], chatId);
+						}
+						}
+					} else if (tokens[0].equals(STRATEGY)) {
+						App app = apps.get(tokens[1]);
+						if (app == null) {
+							sendMessage("Invalid strategy " + tokens[1], chatId);
+						}
+						switch (tokens[2]) {
+						case PAUSE: {
+							app.pause();
+							sendMessage("Straegy " + tokens[1] + " paused by " + fromUser);
+							break;
+						}
+						case RESUME: {
+							app.resume();
+							sendMessage("Straegy " + tokens[1] + " resumed by " + fromUser);
+							break;
+						}
+						default: {
+							sendMessage("Invalid action " + tokens[2], chatId);
+						}
+						}
+					} else {
+						sendMessage("Invalid action object " + tokens[0], chatId);
+
+					}
+				}
+				return UpdatesListener.CONFIRMED_UPDATES_ALL;
+			}
+
+			private boolean isAuthValid(String auth, String fromUser) {
+				try {
+					int otp = Integer.parseInt(auth);
+					return getValidUserIds().contains(fromUser) && GoogleAuthUtils.authorize(fromUser, otp);
+				} catch (Exception e) {
+					Thread.currentThread().setName("TelegramUtils");
+					log.info("Missing otp");
+				}
+				return false;
+			}
+
+			private Set<String> getValidUserIds() {
+				// TODO make this list configurable
+				Set<String> validUsers = new HashSet<>();
+				validUsers.add("mo9001");
+				return validUsers;
+			}
+		});
+
 	}
 }
